93105cf77237a6bdff700f9bbdf863b0132f6702
cyberstud08 <meshubham@duck.com>
2022-03-22 23:19:42 +0530

    html, css completed, js issues

--- JS/index.js
+++ JS/index.js
@@ -22,9 +22,45 @@ let resetBtn = document.querySelector("#reset");
 
 flipBtn.addEventListener("click", () =>{
     let i = Math.floor(Math.random()*2);
-    console.log(i);
+    coin.style.animation = "none";
+    if (i) {
+        setTimeout(function () {
+            coin.style.animation = "spin-heads 1.1s forwards";
+        }, 100);
+        heads++;
+
+    }
+    else {
+        setTimeout(function () {
+            coin.style.animation = "spin-tails 1.1s forwards"
+        }, 100);
+        tails++;
+    }
+
+    //updating head tail count
+    setTimeout(updateStats, 3000);
+    disableButton();
 })
-resetBtn.addEventListener("click", () =>{
-    let i = Math.floor(Math.random()*2);
-    console.log(i);
+
+
+// !  heads & tails count function
+
+function updateStats(){
+    document.querySelector("#heads-count").textContent = 'Heads: ${heads}';
+
+    document.querySelector("#tails-count").textContent = 'Tails: $(tails)';
+}
+
+function disableButton(){
+    flipBtn.disabled = true;
+    setTimeout(function(){
+        flipBtn.disabled = false;
+    }, 3000);
+}
+
+resetBtn.addEventListener("click", () => {
+    coin.style.transform = "rotateX(0)";
+    heads = 0;
+    tails = 0;
+    updateStats();    
 })
